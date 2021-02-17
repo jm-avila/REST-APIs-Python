3cbc550de1523a71fd4bf92e706e71946adc99ce
Jose Miguel Avila Gomez <josemiguelavilagomez@Joses-MacBook-Pro.local>
2021-02-17 19:56:13 +0100

    03_getting_user_input

---
+++ Refresher/03_getting_user_input/code.py
@@ -0,0 +1,6 @@
+size_input = input("How big is your house (in square meters): ")
+
+square_meters = int(size_input)
+square_feets = square_meters * 10.8
+
+print(f"{square_meters} square meters is {square_feets} square feets.")
