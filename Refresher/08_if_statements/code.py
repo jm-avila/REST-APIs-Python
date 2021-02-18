f7d515ad9eb16a51d3f790691e4651fffba93a67
Jose Miguel Avila Gomez <josemiguelavilagomez@Joses-MacBook-Pro.local>
2021-02-18 11:59:28 +0100

    08_if_statements

---
+++ Refresher/08_if_statements/code.py
@@ -0,0 +1,8 @@
+day_of_week = input("What day of the week is it today? ")
+
+if day_of_week == "Monday":
+    print("Have a great start to your week!")
+elif day_of_week == "Friday":
+    print(f"Have a great weekend!") 
+else:
+    print(f"Have a great {day_of_week}!") 
