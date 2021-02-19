df9510ef2233c71502310f653b36c8b514ce0741
Jose Miguel Avila Gomez <josemiguelavilagomez@Joses-MacBook-Pro.local>
2021-02-19 18:17:24 +0100

    09_the_in_keyword

---
+++ Refresher/09_the_in_keyword/code.py
@@ -0,0 +1,9 @@
+movies_watched = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter something you've watched recently: ")
+
+print("in movies_watched", user_movie in movies_watched)
+
+vowels = "aeiou"
+user_letter = input("Enter a letter and see if it's a vowel: ")
+
+print("in vowels", user_letter in vowels)
