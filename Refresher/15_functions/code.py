c45e0f90ca1355ceacaebb00e1263102bc2e326a
Jose Miguel Avila Gomez <josemiguelavilagomez@Joses-MacBook-Pro.local>
2021-02-21 12:31:05 +0100

    15_functions

---
+++ Refresher/15_functions/code.py
@@ -0,0 +1,9 @@
+def hello():
+    print("Hello!")
+
+def user_age_in_seconds():
+    user_age = int(input("Enter your age: "))
+    age_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age in seconds is {age_seconds}s")
+
+user_age_in_seconds()
